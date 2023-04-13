5f86929b312f0056fdd26e1c45ea7efbb6bf5c0c
Ravi Gunasena <ravigunz@gmail.com>
2023-04-13 22:03:51 +0530

    initiate the project

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,27 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class HomeLayout extends Component {
-
-    state = { users: [], 
-    isLoading: true,
-};
-
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json()).then(data => { this.setState({users: data, isLoading: false});
-    }).catch(() => {});
+export default function HomeLayout() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
-
-  render() {
-    return (
-      <div>
-{this.state.isLoading == true ? (<div>{' '} <h2>Loading......</h2>{' '}</div>) : ( <div>Completed</div>)}
-         <h1>Members</h1>
-        <p style={{ textAlign: 'left'}}>
-        {this.state.users.map((val, key) => {return <div key={key}>{val.name}</div>})}
-        </p>
-      </div>
-    );
-  }
-}
-
